dc117203802564a71542e84354a4026c22fbb2c8
tuoixanh0203 <62226586+tuoixanh0203@users.noreply.github.com>
2020-10-04 00:19:01 +0700

    my commit message

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,15 @@ package com.company;
 import java.io.IOException;
 
 public class Main {
+    /**
+     * 
+     * @param args
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
         DictionaryCommandline dicComL = new DictionaryCommandline();
         dicComL.dictionaryAdvanced();
     }
 }
+
 
